63bc52eb3a096d9ecae6800a87f96c9bde0eaa34
nickzznl <nick.sluiters@gmail.com>
2022-06-08 16:13:25 +0200

    First commit

---
+++ AdventOfCode/DayOneSonarSweep.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode
+{
+    public class DayOneSonarSweep
+    {
+        public int CountIncreasesForSonarSweep(string sonarSweepsDistance)
+        {
+            if (sonarSweepsDistance != null)
+            {
+                var sweepsDistanceArray = sonarSweepsDistance.Split(",");
+                var amountOfIncreases = 0;
+
+                for (int i = 1; i < sweepsDistanceArray.Length; i++)
+                {
+                    if(int.Parse(sweepsDistanceArray[i]) > int.Parse(sweepsDistanceArray[i - 1]))
+                    {
+                        amountOfIncreases++;
+                    }
+                }
+
+                return amountOfIncreases;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode/TestDayOneSonarSweep.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode
+{
+    [TestFixture]
+    [Category("Unit")]
+    public class TestDayOneSonarSweep
+    {
+        [Test]
+        [TestCase ("199,200,208,210,200,207,240,269,260,263", 7)]
+        public void TestDayOneSonarSweep_WithCorrectValues_ShouldGetCorrectReturnAmount(string sonarSweeps, int expectedResult)
+        {
+            //arrange
+            var sut = new DayOneSonarSweep();
+
+            //act
+            var actualResult = sut.CountIncreasesForSonarSweep(sonarSweeps);
+
+            //Assert
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+    }
+}
